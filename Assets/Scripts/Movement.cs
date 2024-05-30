c66b7151d295e33f265963a94bc9dca26ec82b95
Erika Stuart (1000106562) <STUAET1@student.op.ac.nz>
2024-05-30 15:20:12 +1200

    Holding keys moves vehicle

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,23 +7,24 @@ public class Movement : MonoBehaviour
 {
     private int speed = 5;
     private Vector2 moveInput;
-    private Vector3 move;
+    private Vector3 direction;
+    private Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.velocity = new Vector3(direction.x * speed, 0, direction.z * speed);
     }
 
     public void Forwards(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
-        move = new Vector3(moveInput.x, 0, moveInput.y);
+        direction = new Vector3(moveInput.x, 0, moveInput.y);
     }
 
 }
