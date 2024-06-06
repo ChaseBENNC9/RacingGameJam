a97062203c9d8bf9d18c622d388b6eb9bdc00c50
ChaseBENNC9 <104808214+ChaseBENNC9@users.noreply.github.com>
2024-06-06 13:00:12 +1200

    add: checkpoint changes color on player trigger enter

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -7,5 +7,39 @@ using UnityEngine.UI;
 
 public class Checkpoint : MonoBehaviour
 {
-    
+    public Material activatedMaterial;
+    public Material defaultMaterial;
+    private bool isActivated = false;
+    public bool IsActivated
+    {
+        get => isActivated;
+        set
+        {
+            isActivated = value;
+            if (isActivated)
+            {
+                transform.GetComponentInChildren<MeshRenderer>().material = activatedMaterial;
+            }
+            else
+            {
+                transform.GetComponent<MeshRenderer>().material = defaultMaterial;
+            }
+        }
+    }
+
+    public float activatedTime = 0.0f;
+
+
+
+
+    void OnTriggerEnter(Collider other)
+{
+    if (other.CompareTag("Player"))
+    {
+        this.IsActivated = true;
+    }
 }
+}
+
+
+
--- Assets/Scripts/Managers/CheckpointManager.cs
+++ Assets/Scripts/Managers/CheckpointManager.cs
@@ -8,5 +8,17 @@ using UnityEngine.UI;
 
 public class CheckpointManager : MonoBehaviour
 {
+    private List<Checkpoint> checkpoints = new();
+
+    void Start()
+    {
+        checkpoints.AddRange(FindObjectsOfType<Checkpoint>());
+        foreach (Checkpoint checkpoint in checkpoints)
+        {
+            checkpoint.IsActivated = false;
+        }
+    }
+
+
 
 }
