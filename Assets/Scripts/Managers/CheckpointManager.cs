a0cbc22ea648d72edeba6c1cc6361a5426c5bf1f
ChaseBENNC9 <104808214+ChaseBENNC9@users.noreply.github.com>
2024-06-06 12:38:26 +1200

    add: new Scripts to be used

---
+++ Assets/Scripts/Managers/CheckpointManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class CheckpointManager : MonoBehaviour
+{
+
+}
