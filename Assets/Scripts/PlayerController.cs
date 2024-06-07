a42c6eecaba07b2d9eefb2557012b64123da1856
ChaseBENNC9 <104808214+ChaseBENNC9@users.noreply.github.com>
2024-06-08 10:54:14 +1200

    doc: added header comment to respawn method

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,9 +14,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody))] //Ensures that the object always has a rigidbody component
+
 public class PlayerController : MonoBehaviour
 {
-    private int speed = 20;
+    private int speed = 20; 
     private int rotationSpeed = 100;
     private Vector2 moveInput;
     private Vector3 direction;
@@ -38,6 +40,7 @@ public class PlayerController : MonoBehaviour
 
     /// <summary>
     /// When the player uses the input system, the player will move.
+    /// </summary>
     public void Forwards(InputAction.CallbackContext context)
     {
         if (GameManager.currentGameState == GameState.Racing)
@@ -47,27 +50,28 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+
+    /// <summary>
+    /// When the player holds the respawn button, the player will respawn at the last checkpoint.
+    /// </summary>
     public void Respawn(InputAction.CallbackContext context)
     {
         if (CheckpointManager.inst.GetLastCheckpoint() != null)
         {
+            
             if (context.started)
             {
-                Debug.Log("Respawn started");
                 fillCircle.SetActive(true);
                 fillCircle.GetComponent<Animation>().Play("FillCircle");
-                Debug.Log("Respawn started");
             }
             if (context.performed)
             {
-
                 gameObject.transform.position = CheckpointManager.inst.GetLastCheckpoint().spawnPoint.position;
                 gameObject.transform.rotation = CheckpointManager.inst.GetLastCheckpoint().gameObject.transform.rotation;
                 fillCircle.SetActive(false);
             }
             if (context.canceled)
             {
-                Debug.Log("Respawn canceled");
                 fillCircle.SetActive(false);
             }
         }
